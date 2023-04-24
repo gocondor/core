ef989f57ce48a49584aa66e95802c32d83683627
Harran Ali <harran.m@gmail.com>
2023-04-25 00:16:45 +0400

    cleanup core.go

--- core.go
+++ core.go
@@ -24,67 +24,46 @@ type App struct {
 	Features *Features
 }
 
-// GORM is a const represents gorm variable name
-const GORM = "gorm"
-
-// CACHE a cache engine variable
-const CACHE = "cache"
-
-// New initiates the app struct
 func New() *App {
 	return &App{
 		Features: &Features{},
 	}
 }
 
-// SetEnv sets environment varialbes
 func (app *App) SetEnv(env map[string]string) {
 	for key, val := range env {
 		os.Setenv(strings.TrimSpace(key), strings.TrimSpace(val))
 	}
 }
 
-// set the logs file path
 func (app *App) SetLogsFilePath(f string) {
 	filePath = f
 }
 
-// get the logs file
 func (app *App) GetLogsFile() *os.File {
 	return logsFile
 }
 
-// Bootstrap initiate app
 func (app *App) Bootstrap() {
-	//initiate middlewares engine varialbe
-	middlewares.New() // TODO rename to initialize
-
-	//initiate routing engine varialbe
+	middlewares.New()
 	NewRouter()
-
-	//initiate data base variable
 	if app.Features.Database == true {
-		database.New() // TODO rename to initialize
+		database.New()
 	}
-
-	// initiate the cache varialbe
 	if app.Features.Cache == true {
-		cache.New(app.Features.Cache) // TODO rename to initialize
+		cache.New(app.Features.Cache)
 	}
 }
 
-// Start GoCondor
 func (app *App) Run(portNumber string, router *httprouter.Router) {
 	router = app.RegisterRoutes(ResolveRouter().GetRoutes(), router)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", portNumber), router))
 }
 
-// SetEnabledFeatures to control what features to turn on or off
 func (app *App) SetEnabledFeatures(features *Features) {
 	app.Features = features
 }
 
-// RegisterRoutes register routes on gin engine
 func (app *App) RegisterRoutes(routes []Route, router *httprouter.Router) *httprouter.Router {
 	for _, route := range routes {
 		switch route.Method {
@@ -108,7 +87,6 @@ func (app *App) RegisterRoutes(routes []Route, router *httprouter.Router) *httpr
 	return router
 }
 
-// make handler func
 func makeHTTPRouterHandlerFunc(h Handler) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		ctx := &Context{
@@ -128,7 +106,7 @@ func makeHTTPRouterHandlerFunc(h Handler) httprouter.Handle {
 		for _, header := range response.headers {
 			w.Header().Add(header.key, header.val)
 		}
-		defer logsFile.Close() // close file after handle
+		defer logsFile.Close()
 		if response.getTextBody() != "" {
 			w.Header().Add("Content-Type", "text/html; charset=utf-8")
 			response.responseWriter.Write([]byte(response.getTextBody()))

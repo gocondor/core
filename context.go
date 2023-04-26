38c15eca069a9d93a0c6ebdcdf68a813d9463909
Harran Ali <harran.m@gmail.com>
2023-04-26 14:05:04 +0400

    add debug function

--- context.go
+++ context.go
@@ -1,7 +1,22 @@
 package core
 
+import (
+	"fmt"
+	"reflect"
+)
+
 type Context struct {
 	Request  *Request
 	Response *Response
 	Logger   *Logger
+}
+
+func (c *Context) Debug(d interface{}) {
+	m := reflect.ValueOf(d)
+	if m.Kind() == reflect.Pointer {
+		m = m.Elem()
+	}
+	formatted := fmt.Sprintf("Type: %T | underlaying type: %v | value: %v", d, m.Kind(), d)
+	fmt.Println(formatted)
+	c.Response.responseWriter.Write([]byte(formatted))
 }

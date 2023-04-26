c6513417fc7d33f8f5fcc2daa591832201ee1229
Harran Ali <harran.m@gmail.com>
2023-04-26 13:35:37 +0400

    update the response

--- response.go
+++ response.go
@@ -1,6 +1,9 @@
 package core
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type Response struct {
 	headers        []header
@@ -14,11 +17,11 @@ type header struct {
 	val string
 }
 
-func (rs *Response) setTextBody(body string) {
-	rs.textBody = body
+func (rs *Response) WriteText(body interface{}) {
+	rs.textBody = rs.castBasicVarsToString(body)
 }
 
-func (rs *Response) setJsonBody(body string) {
+func (rs *Response) WriteJson(body string) {
 	rs.jsonBody = body
 }
 
@@ -30,7 +33,7 @@ func (rs *Response) getJsonBody() string {
 	return rs.jsonBody
 }
 
-func (rs *Response) setHeader(key string, val string) {
+func (rs *Response) SetHeader(key string, val string) {
 	h := header{
 		key: key,
 		val: val,
@@ -38,7 +41,63 @@ func (rs *Response) setHeader(key string, val string) {
 	rs.headers = append(rs.headers, h)
 }
 
-func (rs *Response) getHeader(key string) string {
+func (rs *Response) getHeaders() []header {
+	return rs.headers
+}
 
-	return ""
+func (rs *Response) castBasicVarsToString(data interface{}) string {
+	switch dataType := data.(type) {
+	case string:
+		return fmt.Sprintf("%v", data)
+	case []byte:
+		d := data.(string)
+		return fmt.Sprintf("%v", d)
+	case int:
+		intVar, _ := data.(int)
+		return fmt.Sprintf("%v", intVar)
+	case int8:
+		int8Var := data.(int8)
+		return fmt.Sprintf("%v", int8Var)
+	case int16:
+		int16Var := data.(int16)
+		return fmt.Sprintf("%v", int16Var)
+	case int32:
+		int32Var := data.(int32)
+		return fmt.Sprintf("%v", int32Var)
+	case int64:
+		int64Var := data.(int64)
+		return fmt.Sprintf("%v", int64Var)
+	case uint:
+		uintVar, _ := data.(uint)
+		return fmt.Sprintf("%v", uintVar)
+	case uint8:
+		uint8Var := data.(uint8)
+		return fmt.Sprintf("%v", uint8Var)
+	case uint16:
+		uint16Var := data.(uint16)
+		return fmt.Sprintf("%v", uint16Var)
+	case uint32:
+		uint32Var := data.(uint32)
+		return fmt.Sprintf("%v", uint32Var)
+	case uint64:
+		uint64Var := data.(uint64)
+		return fmt.Sprintf("%v", uint64Var)
+	case float32:
+		float32Var := data.(float32)
+		return fmt.Sprintf("%v", float32Var)
+	case float64:
+		float64Var := data.(float64)
+		return fmt.Sprintf("%v", float64Var)
+	case complex64:
+		complex64Var := data.(complex64)
+		return fmt.Sprintf("%v", complex64Var)
+	case complex128:
+		complex128Var := data.(complex128)
+		return fmt.Sprintf("%v", complex128Var)
+	case bool:
+		boolVar := data.(bool)
+		return fmt.Sprintf("%v", boolVar)
+	default:
+		panic(fmt.Sprintf("unsupported data type %v!", dataType))
+	}
 }

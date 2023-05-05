2ef3678e0df102f7504df4135a67ff97954871ff
Harran Ali <harran.m@gmail.com>
2023-05-05 12:38:28 +0400

    test middlewares

---
+++ middlewares_test.go
@@ -0,0 +1,64 @@
+// Copyright 2021 Harran Ali <harran.m@gmail.com>. All rights reserved.
+// Use of this source code is governed by MIT-style
+// license that can be found in the LICENSE file.
+
+package core
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestNewMiddlewares(t *testing.T) {
+	mw := NewMiddlewares()
+	if fmt.Sprintf("%T", mw) != "*core.Middlewares" {
+		t.Errorf("failed testing new middleware")
+	}
+}
+
+func TestResloveMiddleWares(t *testing.T) {
+	NewMiddlewares()
+	mw := ResolveMiddlewares()
+	if fmt.Sprintf("%T", mw) != "*core.Middlewares" {
+		t.Errorf("failed resolve middlewares")
+	}
+}
+
+func TestAttach(t *testing.T) {
+	mw := NewMiddlewares()
+	tmw := func(c *Context) {
+		c.Logger.Info("Testing!")
+	}
+	mw.Attach(tmw)
+	mws := mw.getByIndex(0)
+	if reflect.ValueOf(tmw).Pointer() != reflect.ValueOf(mws).Pointer() {
+		t.Errorf("Failed testing attach middleware")
+	}
+}
+
+func TestGetMiddleWares(t *testing.T) {
+	mw := NewMiddlewares()
+	t1 := func(c *Context) {
+		c.Logger.Info("testing1!")
+	}
+	t2 := func(c *Context) {
+		c.Logger.Info("testing2!")
+	}
+	mw.Attach(t1)
+	mw.Attach(t2)
+	if len(mw.GetMiddlewares()) != 2 {
+		t.Errorf("failed testing get middlewares")
+	}
+}
+
+func TestGetByIndex(t *testing.T) {
+	mw := NewMiddlewares()
+	t1 := func(c *Context) {
+		c.Logger.Info("testing!")
+	}
+	mw.Attach(t1)
+	if reflect.ValueOf(mw.getByIndex(0)).Pointer() != reflect.ValueOf(t1).Pointer() {
+		t.Errorf("failed testing get by index")
+	}
+}

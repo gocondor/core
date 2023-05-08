232d7ef2731fac82764da2fdb396e8f725a5ca60
Harran Ali <harran.m@gmail.com>
2023-05-08 20:56:07 +0400

    update context

--- context.go
+++ context.go
@@ -5,8 +5,16 @@
 package core
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"os"
+	"path"
+	"path/filepath"
 	"reflect"
+	"strings"
+
+	"github.com/google/uuid"
 )
 
 type Context struct {
@@ -61,9 +69,77 @@ func (c *Context) RequestParamExists(key string) bool {
 	return c.Request.HttpRequest.Form.Has(key)
 }
 
-// TODO implement
-func (c *Context) GetRequestFile(name string) {
+func (c *Context) GetUploadedFile(name string) *UploadedFileInfo {
 	file, fileHeader, _ := c.Request.HttpRequest.FormFile(name)
-	c.LogInfo(fileHeader.Filename)
-	fmt.Println(file)
+	defer file.Close()
+	ext := strings.TrimPrefix(path.Ext(fileHeader.Filename), ".")
+	tmpFilePath := filepath.Join(os.TempDir(), uuid.NewString()+"."+ext)
+	tmpFile, _ := os.Create(tmpFilePath)
+	buff := make([]byte, 100)
+	for {
+		n, err := file.Read(buff)
+		if err != nil && err != io.EOF {
+			panic("error with uploaded file")
+		}
+		if n == 0 {
+			break
+		}
+		n, _ = tmpFile.Write(buff[:n])
+	}
+	tmpFileInfo, _ := os.Stat(tmpFilePath)
+	defer tmpFile.Close()
+	uploadedFileInfo := &UploadedFileInfo{
+		FullPath:             tmpFilePath,
+		Name:                 fileHeader.Filename,
+		NameWithoutExtension: strings.TrimSuffix(fileHeader.Filename, path.Ext(fileHeader.Filename)),
+		Extension:            ext,
+		Size:                 int(tmpFileInfo.Size()),
+	}
+	return uploadedFileInfo
+}
+
+func (c *Context) MoveFile(sourceFilePath string, destFolderPath string) error {
+	os.MkdirAll(destFolderPath, 644)
+	srcFileInfo, err := os.Stat(sourceFilePath)
+	if err != nil {
+		return err
+	}
+	if !srcFileInfo.Mode().IsRegular() {
+		return errors.New("can not copy file, not in a regular mode")
+	}
+	srcFile, err := os.Open(sourceFilePath)
+	if err != nil {
+		return err
+	}
+	defer srcFile.Close()
+	destFilePath := filepath.Join(destFolderPath, srcFileInfo.Name())
+	fmt.Println(srcFileInfo.Name())
+	destFile, err := os.OpenFile(destFilePath, os.O_CREATE|os.O_RDWR|os.O_APPEND, 744)
+	if err != nil {
+		return err
+	}
+	defer destFile.Close()
+	buff := make([]byte, 100)
+	for {
+		n, err := srcFile.Read(buff)
+		if err != nil && err != io.EOF {
+			panic("error moving file")
+		}
+		if n == 0 {
+			break
+		}
+		_, err = destFile.Write(buff)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+type UploadedFileInfo struct {
+	FullPath             string
+	Name                 string
+	NameWithoutExtension string
+	Extension            string
+	Size                 int
 }

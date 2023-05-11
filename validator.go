dc5970b987acedd80464f8ffa85bf882408a30ae
Harran Ali <harran.m@gmail.com>
2023-05-11 10:17:10 +0400

    update validator

--- validator.go
+++ validator.go
@@ -3,8 +3,10 @@ package core
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	validation "github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
 )
 
 type Validator struct{}
@@ -28,12 +30,15 @@ func resolve() *Validator {
 }
 
 func (v *Validator) Validate(data ValidatorData, rules ValidatorRules) validationResult {
-	// TODO map rules
 	vr = validationResult{}
 	vr.hasFailed = false
 	res := map[string]string{}
 	for key, val := range data {
-		err := validation.Validate(val, validation.Required)
+		_, ok := rules[key]
+		if !ok {
+			continue
+		}
+		err := validation.Validate(val, parseRules(rules[key])...)
 		if err != nil {
 			res[key] = fmt.Sprintf("%v: %v", key, err.Error())
 
@@ -60,4 +65,89 @@ func (vr *validationResult) GetErrorMessagesJson() string {
 		panic("error converting validation error messages to json")
 	}
 	return string(j)
+}
+
+func parseRules(rawRules interface{}) []validation.Rule {
+	var res []validation.Rule
+	rulesStr, ok := rawRules.(string)
+	if !ok {
+		panic("invalid validation rule")
+	}
+	rules := strings.Split(rulesStr, "|")
+	for _, rule := range rules {
+		rule = strings.TrimSpace(rule)
+		res = append(res, getRule(rule))
+	}
+
+	return res
+}
+
+// TODO handle all rules
+func getRule(rule string) validation.Rule {
+	switch rule {
+	case "required":
+		return validation.Required
+	case "email":
+		return is.Email
+	case "url":
+		return is.URL
+	case "alpha":
+		return is.Alpha
+	case "digit":
+		return is.Digit
+	case "alphaNumeric":
+		return is.Alphanumeric
+	case "lowerCase":
+		return is.LowerCase
+	case "upperCase":
+		return is.UpperCase
+	case "int":
+		return is.Int
+	case "float":
+		return is.Float
+	case "uuid":
+		return is.UUID
+	case "creditCard":
+		return is.CreditCard
+	case "json":
+		return is.JSON
+	case "base64":
+		return is.Base64
+	case "countryCode2":
+		return is.CountryCode2
+	case "countryCode3":
+		return is.CountryCode3
+	case "isoCurrencyCode":
+		return is.CurrencyCode
+	case "mac":
+		return is.MAC
+	case "ip":
+		return is.IP
+	case "ipv4":
+		return is.IPv4
+	case "ipv6":
+		return is.IPv6
+	case "subdomain":
+		return is.Subdomain
+	case "domain":
+		return is.Domain
+	case "dnsName":
+		return is.DNSName
+	case "host":
+		return is.Host
+	case "port":
+		return is.Port
+	case "mongoDbId":
+		return is.MongoID
+	case "latitude":
+		return is.Latitude
+	case "longitude":
+		return is.Longitude
+	case "ssn":
+		return is.SSN
+	case "semver":
+		return is.Semver
+	default:
+		panic(fmt.Sprintf("invalid validation rule: %v", rule))
+	}
 }
